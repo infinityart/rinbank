e4a682e8e51310588608b17a41dd5787489b8db1
Jonty <jonty_wolf@hotmail.com>
2018-09-09 14:53:03 +0200

    Init

---
+++ app/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @author: Jonty Sponselee <jsponselee@student.scalda.nl>
+ * @since: 9/9/2018
+ */
+declare(strict_types = 1);
+
+echo 'Hello World!';
---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @author: Jonty Sponselee <jsponselee@student.scalda.nl>
+ * @since: 9/9/2018
+ */
+
+declare(strict_types = 1);
+
+require __DIR__ . '/../app/bootstrap.php';
